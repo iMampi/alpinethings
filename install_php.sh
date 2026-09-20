66d48867d9c7b66701b68bbdcb6d1604aa4c807e
IARIMAMPIANINA RAMANANDRAIBE Mampiniana Ismaël <68663962+iMampi@users.noreply.github.com>
2026-09-20 21:23:37 +0200

    Update install_php.sh

--- install_php.sh
+++ install_php.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e 
 apk add php84 php84-bcmath php84-bz2 php84-ctype php84-curl php84-dom php84-enchant php84-exif php84-fpm php84-gd php84-gettext php84-gmp php84-iconv php84-imap php84-intl php84-json php84-mbstring php84-opcache php84-openssl php84-phar php84-posix php84-pspell php84-recode php84-session php84-simplexml php84-sockets php84-sysvmsg php84-sysvsem php84-sysvshm php84-tidy php84-xml php84-xmlreader php84-xmlrpc php84-xmlwriter php84-xsl php84-zip php84-sqlite3
+
+apk add php84-pgsql php84-mysqli php84-mysqlnd php84-snmp php84-soap php84-ldap php84-pcntl php84-pear php84-shmop php84-wddx php84-cgi php84-pdo php84-snmp php84-tokenizer
